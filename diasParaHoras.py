26153fefe2e0f2719af5a96805906b558362bfaf
vitorsilva240 <vitorcarvallho7744@gmail.com>
2026-06-18 15:53:46 -0300

    feat: D_HR

---
+++ diasParaHoras.py
@@ -0,0 +1,7 @@
+def diasParaHoras():
+    dias = int (input("Digite a quantidade de dias para converter em horas:"))
+    horas = dias * 24
+
+    print(f"{dias} convertidos em horas é igual a {horas} horas.")
+    
+diasParaHoras()
